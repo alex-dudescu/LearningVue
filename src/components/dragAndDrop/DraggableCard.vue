eac053f9fada6bbd1de98cad51c6b640aa6c5f9f
Alexandru Dudescu <alexdudescu@hotmail.com>
2019-03-22 09:00:33 +0200

    Clean unnecessary files
    - Remove DraggableCard component
    - Remove old drag and drop store module

--- src/components/dragAndDrop/DraggableCard.vue
+++
@@ -1,3 +0,0 @@
-<template src="./DraggableCard/DraggableCard.html"></template>
-<style lang="scss" scoped src="./DraggableCard/DraggableCard.scss"></style>
-<script src="./DraggableCard/DraggableCard.js"></script>
